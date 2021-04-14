e5073079d946d6f0bb1e434532ed90a0d9478307
Gaurav <gaurav_rehan@hotmail.com>
2021-04-14 21:51:06 +0530

    first commit

--- src/main/java/com/ms/gaurav/gauravhelloworld/HelloworldController.java
+++ src/main/java/com/ms/gaurav/gauravhelloworld/HelloworldController.java
@@ -3,11 +3,21 @@ package com.ms.gaurav.gauravhelloworld;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 public class HelloworldController {
 
     @GetMapping("/messages")
     public String getMessage() {
-        return "Hello world from spring boot docker running over AKS";
+        StringBuilder sb = new StringBuilder();
+        sb.append("<b> Hello world from spring boot docker running over Azure AKS - Gaurav Rehan </b> <BR><BR>" );
+        sb.append("<b>Env Vars: </b> <BR>");
+        for (Map.Entry<String, String> entry : System.getenv().entrySet()) {
+            sb.append("<b>"+entry.getKey() + " : </b>" + entry.getValue() + " <BR>");
+        }
+        return sb.toString();
     }
+
+
 }
